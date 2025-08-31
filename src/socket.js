d812a5719d68b397d630b2658a8c1fb1476082bf
1ayush-ilu <av3869712@gmail.com>
2025-08-31 13:19:30 +0530

    fix: update env and socket config

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const backendUrl = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
-const socket = io(backendUrl, {
+const socket = io("https://backend-hpga.onrender.com", {
   transports: ["websocket"],
   withCredentials: true,
 });
